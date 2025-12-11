bf847c09634160da40478213e4578f25ef62ab22
Necati Arslan <necatia@gmail.com>
2025-12-11 15:41:45 -0500

    bismillah

---
+++ justfile
@@ -0,0 +1,29 @@
+package:
+    vsce package
+    mv *.vsix ./vsix/
+
+build:
+    vsce package
+    mv *.vsix ./vsix/
+
+publish:
+    vsce publish
+
+npm-doctor:
+    npm doctor # check dependencies
+    npm prune # remove unused dependencies
+    npx depcheck # check dependencies
+    npm-check # check dependencies
+
+
+npm-outdated:
+    npm outdated
+    npx npm-check-updates
+
+npm-update:
+    npm update
+
+npm-install:
+    rm -rf node_modules package-lock.json
+    npm install
+    npx tsc --noEmit
---
+++ src/extension.ts
@@ -0,0 +1,67 @@
+import * as vscode from 'vscode';
+import * as ui from './access/UI';
+import * as StatusBar from './access/StatusBarItem';
+
+export function activate(context: vscode.ExtensionContext) {
+	ui.logToOutput('Aws AI Assistant is now active!');
+
+	
+	new StatusBar.StatusBarItem(context);
+	
+	vscode.commands.registerCommand('aws-ai-assistant.RefreshCredentials', () => {
+		StatusBar.StatusBarItem.Current.GetCredentials();
+	});
+
+	vscode.commands.registerCommand('aws-ai-assistant.SetAwsLoginCommand', () => {
+		StatusBar.StatusBarItem.Current.SetAwsLoginCommand();
+	});
+
+	vscode.commands.registerCommand('aws-ai-assistant.ListAwsProfiles', () => {
+		StatusBar.StatusBarItem.Current.ListAwsProfiles();
+	});
+
+	vscode.commands.registerCommand('aws-ai-assistant.RunLoginCommand', () => {
+		StatusBar.StatusBarItem.Current.RunLoginCommand();
+	});
+
+	vscode.commands.registerCommand('aws-ai-assistant.PauseAutoLogin', () => {
+		StatusBar.StatusBarItem.Current.PauseAutoLogin();
+	});
+
+	vscode.commands.registerCommand('aws-ai-assistant.SetActiveProfile', () => {
+		StatusBar.StatusBarItem.Current.SetActiveProfile();
+	});
+
+	vscode.commands.registerCommand('aws-ai-assistant.ShowActiveCredentials', () => {
+		StatusBar.StatusBarItem.Current.ShowActiveCredentials();
+	});
+
+	vscode.commands.registerCommand('aws-ai-assistant.ShowDefaultCredentials', () => {
+		StatusBar.StatusBarItem.Current.ShowDefaultCredentials();
+	});
+
+	vscode.commands.registerCommand('aws-ai-assistant.OpenCredentialsFile', () => {
+		StatusBar.StatusBarItem.Current.OpenCredentialsFile();
+	});
+
+	vscode.commands.registerCommand('aws-ai-assistant.OpenConfigFile', () => {
+		StatusBar.StatusBarItem.Current.OpenConfigFile();
+	});
+
+	vscode.commands.registerCommand('aws-ai-assistant.TestAwsConnectivity', () => {
+		StatusBar.StatusBarItem.Current.TestAwsConnectivity();
+	});
+
+	vscode.commands.registerCommand('aws-ai-assistant.CopyCredentialsToDefaultProfile', () => {
+		StatusBar.StatusBarItem.Current.CopyCredentialsToDefaultProfile();
+	});
+
+	vscode.window.onDidCloseTerminal((terminal) => {
+			StatusBar.StatusBarItem.Current.onDidCloseTerminal(terminal);
+	});
+
+}
+
+export function deactivate() {
+	ui.logToOutput('Aws AI Assistant is now de-active!');
+}
